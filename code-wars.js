718a3635f3b848ee9f58a38536049d5e4e0253a4
ANTHONY WOO <anthony.woo14@gmail.com>
2023-02-10 17:31:53 -0700

    added another solution

--- code-wars.js
+++ code-wars.js
@@ -9,3 +9,23 @@ function findOutlier(integers) {
         ? integers.filter((int) => int % 2 === 0)[0]
         : integers.filter((int) => !(int % 2 === 0))[0];
 }
+
+// take an array of numbers and all the zeros to the end of the number
+function moveZeros(arr) {
+    return arr.filter((a) => !(a === 0)).concat(arr.filter((a) => a === 0));
+}
+
+// determine total perimeter of squares within a rectangle (fib sequence)
+function perimeter(n) {
+    let array = [0, 1];
+    let perim = 0;
+    for (let i = 0; i < n; i++) {
+        array.push(array[i] + array[i + 1]);
+    }
+    return (
+        array.reduce((acc, curr) => {
+            acc += curr;
+            return acc;
+        }, 0) * 4
+    );
+}
